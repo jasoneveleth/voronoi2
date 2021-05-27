93ddeb5e27fa43793ceb08d84285c296c482557b
Jason Eveleth <jj11@eveleth.com>
2021-05-27 13:56:33 -0400

    a fix for those who don't have . in $PATH

--- tests/main_test.sh
+++ tests/main_test.sh
@@ -10,7 +10,7 @@ for file in tests/sites/*.in; do
     file_no_extension="$(echo "$file" | cut -f1 -d'.')"
     printf "testing $file_no_extension: "
 
-    voronoi "${file_no_extension}.in" > tmp
+    ./voronoi "${file_no_extension}.in" > tmp
 
     if cmp --silent "${file_no_extension}.out" tmp; then
         printf "${GRN}PASSED${CLR}\n"
