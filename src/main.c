f0c9bafff9dca447479e406281c892ed418ecef7
Jason Eveleth <jj11@eveleth.com>
2021-06-05 12:49:27 -0400

    refactor read_sites_from_file

--- src/main.c
+++ src/main.c
@@ -22,30 +22,28 @@
 static const float alpha = (float)3e-3;
 
 static inline void
-read_sites_from_file(const char *path, point **arr_ptr, int32_t *length)
+read_sites_from_file(const char *path, point **arr_ptr, int32_t *nsites)
 {
     FILE *file = fopen(path, "r");
     if (file == NULL) {
-        fprintf(stderr, "path not valid: %s\n", path);
+        fprintf(stderr, "path not valid: '%s'\n", path);
         exit(1);
     }
     char line[LINELEN];
     point *arr = malloc(2 * sizeof(point));
     int32_t allocated = 2;
-    int nsites;
-    for (nsites = 0; fgets(line, LINELEN, file) != NULL; nsites++) {
-        if (nsites >= allocated) {
+    for ((*nsites) = 0; fgets(line, LINELEN, file) != NULL; (*nsites)++) {
+        if ((*nsites) >= allocated) {
             allocated *= 2;
             arr = realloc(arr, sizeof(point) * (size_t)(allocated));
         }
         char *first = strtok(line, ", \t");
         char *second = strtok(NULL, ", \t");
-        arr[nsites].x = strtof(first, NULL);
-        arr[nsites].y = strtof(second, NULL);
+        arr[(*nsites)].x = strtof(first, NULL);
+        arr[(*nsites)].y = strtof(second, NULL);
     }
     fclose(file);
-    arr = realloc(arr, (size_t)nsites * sizeof(point));
-    *length = nsites;
+    arr = realloc(arr, (size_t)(*nsites) * sizeof(point));
     *arr_ptr = arr;
 }
 
