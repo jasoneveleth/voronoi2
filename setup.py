77f6afc855e3021cdc0bc3f589be7cab83460868
Jason Eveleth <jj11@eveleth.com>
2021-06-09 12:56:00 -0400

    factored out macros from setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ import numpy
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
+macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),('NMAIN',)]
+
 files = ["voronoi.pyx", "src/bintree.c", "src/edgelist.c", "src/fortunes.c", "src/geometry.c", "src/gradient.c", "src/heap.c", "src/main.c"]
 extensions = [Extension("voronoi",
                  files,
                  include_dirs=[numpy.get_include()],
-                 define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),('NMAIN',)]
+                 define_macros=macros
                  )]
 
 setup(
