0c9f5fa9fce3d34b37c9947513deca09ee5b7b6b
Jason Eveleth <jj11@eveleth.com>
2021-06-24 18:29:53 -0400

    comment out gnuplot

--- src/main.c
+++ src/main.c
@@ -208,35 +208,38 @@ main(int argc, char **argv)
         gradient_descent(arrs, (int)nsites,
                          (int)(linesegs_per_trial * pts_per_lineseg));
 
-        FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
-        fputs("set terminal gif animate delay 20\n", gnuplotPipe);
-        fputs("set output 'newest.gif'\n", gnuplotPipe);
-        fputs(
-            "set style line 1 linecolor rgb '#0060ad' linetype 1 linewidth 1 "
-            "pointsize 0\n",
-            gnuplotPipe);
+        // FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
+        // fputs("set terminal gif animate delay 20\n", gnuplotPipe);
+        // fputs("set output 'newest.gif'\n", gnuplotPipe);
+        // fputs(
+        //     "set style line 1 linecolor rgb '#0060ad' linetype 1 linewidth 1 "
+        //     "pointsize 0\n",
+        //     gnuplotPipe);
 
-        fputs("set xrange [0:1]\n", gnuplotPipe);
-        fputs("set yrange [0:1]\n", gnuplotPipe);
-        //  4 becuase we have each line segment on there twice, and its 2 points
-        //  in a lineseg
-        size_t inner_loop_size = 4 * (3 * nsites - 6);
-        for (size_t i = 0; i < ntrials; i++) {
-            fputs("plot '-' with linespoints linestyle 1\n", gnuplotPipe);
-            for (size_t j = 0; j < inner_loop_size; j += 2) {
-                point start =
-                    ((point *)(arrs.linesegs_to_be_cast))[i * inner_loop_size
-                                                          + j];
-                point end =
-                    ((point *)(arrs.linesegs_to_be_cast))[i * inner_loop_size
-                                                          + j + 1];
-                fprintf(gnuplotPipe, "%f %f\n%f %f\n\n", (double)start.x,
-                        (double)start.y, (double)end.x, (double)end.y);
-            }
-            fputs("e\n", gnuplotPipe);
-        }
-        fprintf(gnuplotPipe, "exit");
-        pclose(gnuplotPipe);
+        // fputs("set xrange [0:1]\n", gnuplotPipe);
+        // fputs("set yrange [0:1]\n", gnuplotPipe);
+        // //  4 becuase we have each line segment on there twice, and its 2 points
+        // //  in a lineseg
+        // size_t inner_loop_size = 4 * (3 * nsites - 6);
+        // for (size_t i = 0; i < ntrials; i++) {
+        //     fputs("plot '-' with linespoints linestyle 1\n", gnuplotPipe);
+        //     for (size_t j = 0; j < inner_loop_size; j += 2) {
+        //         point start =
+        //             ((point *)(arrs.linesegs_to_be_cast))[i * inner_loop_size
+        //                                                   + j];
+        //         point end =
+        //             ((point *)(arrs.linesegs_to_be_cast))[i * inner_loop_size
+        //                                                   + j + 1];
+        //         fprintf(gnuplotPipe, "%f %f\n%f %f\n\n", (double)start.x,
+        //                 (double)start.y, (double)end.x, (double)end.y);
+        //     }
+        //     fputs("e\n", gnuplotPipe);
+        // }
+        // fprintf(gnuplotPipe, "exit");
+        // pclose(gnuplotPipe);
+        //
+        // -----------------------------=============-------------------------
+        //
         // for (int i = 0; i < ntrials; i++) { printf("%f\n",
         // arrs.perimeter[i]); } printf("\n");
         // size_t len = ntrials * linesegs_per_trial * pts_per_lineseg
