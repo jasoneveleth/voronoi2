16375f99ba0d88b6cc8f97dfda566b4dbdc29d8a
Jason Eveleth <jj11@eveleth.com>
2021-06-29 09:41:31 -0400

    test sh to bash array -- broke tests ??

--- tests/main_test.sh
+++ tests/main_test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # I assume: cwd == project directory
 # I also assume that everything compiled (i.e. this was called in the make file)
@@ -12,46 +12,34 @@ if [ "" != "$1" ]; then
     printf "voronoi -n 50 [args]; python plot.py -s -t | md5sum > \"tests/sites/${1}.gradout\"\n"
 fi
 
+check_tmp() {
+    if cmp --silent "$1" tests/tmp_file; then
+        printf "${GRN}PASSED${CLR}\n"
+    else
+        printf "${RED}FAILED${CLR}\n"
+    fi
+}
+
 # ========================== ./voronoi <file> ==============================
 for file in tests/sites/*.in; do
     file_no_extension="$(echo "$file" | cut -f1 -d'.')"
     printf "testing $file_no_extension: "
 
     voronoi -n 0 -i "${file_no_extension}.in" | md5sum > tests/tmp_file
-
-    if cmp --silent "${file_no_extension}.out" tests/tmp_file; then
-        printf "${GRN}PASSED${CLR}\n"
-    else
-        printf "${RED}FAILED${CLR}\n"
-    fi
+    check_tmp "${file_no_extension}.out"
 done
 
 # =========================== gradient descent =============================
 cp tests/sites/hundred_point.gradin input
 
-#-----
-file_no_extension='tests/sites/hundredpoint_obj=perimeter'
-printf "testing ${file_no_extension} "
-voronoi -n 50
-#-----
-.env/bin/python plot.py -s -t | md5sum > tests/tmp_file
-if cmp --silent "${file_no_extension}.gradout" tests/tmp_file; then
-    printf "${GRN}PASSED${CLR}\n"
-else
-    printf "${RED}FAILED${CLR}\n"
-fi
+files=("tests/sites/hundredpoint_obj=perimeter" "tests/sites/hundredpoint_obj=perimeter_repel")
+args=("" "-o pr")
 
+for i in ${!files[@]}; do
+    printf "testing ${files[$i]} "
+    voronoi -n 50 ${args[$i]}
+    .env/bin/python plot.py -s -t | md5sum > tests/tmp_file
+    check_tmp "${files[$i]}.gradout"
+    rm -f tests/tmp_file
+done
 
-#----
-file_no_extension='tests/sites/hundredpoint_obj=perimeter_repel'
-printf "testing ${file_no_extension} "
-voronoi -n 50 -o pr
-#----
-.env/bin/python plot.py -s -t | md5sum > tests/tmp_file
-if cmp --silent "${file_no_extension}.gradout" tests/tmp_file; then
-    printf "${GRN}PASSED${CLR}\n"
-else
-    printf "${RED}FAILED${CLR}\n"
-fi
-
-rm -f tests/tmp_file
